6121be50fe072de040dc84336defd934ff9928ad
Gaurav Gangwar <120463239+Gocoderunav@users.noreply.github.com>
2024-03-31 22:19:10 +0530

    Time: 157 ms (11.42%) | Memory: 73.6 MB (21.78%) - LeetSync

--- 148-sort-list/sort-list.cpp
+++ 148-sort-list/sort-list.cpp
@@ -9,67 +9,67 @@
  * };
  */
 class Solution {
+
 private:
-    // find middle of LL using slow and fast pointers
-    ListNode* findLLMid(ListNode* head) {
+    ListNode* findMid(ListNode* head){
         ListNode* slow = head;
-        ListNode* fast = head->next;
-        while (fast != NULL && fast->next != NULL) {
+        ListNode* fast =head->next;
+
+        while(fast && fast->next){
             slow = slow->next;
             fast = fast->next->next;
+
         }
         return slow;
     }
 
-    // merges two sorted LLs
-    ListNode* merge(ListNode* head1, ListNode* head2) {
+    ListNode* merge(ListNode* head1 ,ListNode* head2){
         ListNode* dummy = new ListNode();
         ListNode* p = dummy;
-        ListNode* p1 = head1;
-        ListNode* p2 = head2;
+        ListNode* p1 =head1;
+        ListNode* p2 =head2;
 
-        while (p1 != NULL && p2 != NULL) {
-            if (p1->val < p2->val) {
-                ListNode* temp = p1->next;
-                p->next = p1;
-                p1->next = NULL;
-                p1 = temp;
-            } else {
-                ListNode* temp = p2->next;
+        while(p1 != NULL && p2 != NULL){
+            if(p1->val < p2->val){
+                ListNode* poneNext = p1->next;
+                p->next =p1;
+                
+                p1->next= NULL;
+                p1= poneNext;
+
+            } else{
+
+                ListNode* ptwoNext = p2->next;
                 p->next = p2;
                 p2->next = NULL;
-                p2 = temp;
+                p2= ptwoNext;
+                
             }
             p = p->next;
-        }
 
-        if (p1 == NULL) {
-            p->next = p2;
-        } else if (p2 == NULL) {
-            p->next = p1;
-        }
+            if(p1==NULL){
+                p->next = p2;
+            }
+            if(p2==NULL){
+                p->next = p1;
+            }
 
+        }
         return dummy->next;
-    }
+    };
 public:
-    // merge sort implementation
     ListNode* sortList(ListNode* head) {
-        // base cases
-        if (head == NULL)
-            return NULL;
-        if (head->next == NULL) 
-            return head;
-        
-        // split LL into two halves
-        ListNode* middleOfLL = findLLMid(head);
-        ListNode* leftHalf = head;
-        ListNode* rightHalf = middleOfLL->next;
-        middleOfLL->next = NULL; // this cuts off the right half from the left half
+        if(head == nullptr ) return nullptr;
+        if(head->next == nullptr) return head;
 
-        // sort the two halves seperately and then merge them into one 
-        leftHalf = sortList(leftHalf); 
-        rightHalf = sortList(rightHalf);
-        head = merge(leftHalf, rightHalf);
+        ListNode* middleOfLL = findMid(head);
+        ListNode* leftHalf =   head;
+        ListNode* rightHalf=   middleOfLL->next;
+        middleOfLL->next = nullptr;
+
+        leftHalf = sortList(leftHalf);
+        rightHalf= sortList(rightHalf);
+        head = merge(leftHalf,rightHalf);
 
         return head;
     }
