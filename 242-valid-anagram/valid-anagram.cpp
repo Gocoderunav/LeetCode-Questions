3d5e20a978727daab2556888dd9506f01eb5934f
Gaurav Gangwar <120463239+Gocoderunav@users.noreply.github.com>
2024-05-23 20:13:18 +0530

    Time: 4 ms (90.91%) | Memory: 8.5 MB (61.08%) - LeetSync

--- 242-valid-anagram/valid-anagram.cpp
+++ 242-valid-anagram/valid-anagram.cpp
@@ -1,13 +1,15 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-       vector<int> hashmap1 (26,0);
+         vector<int> hashmap1 (26,0);
        vector<int> hashmap2 (26,0);
-       if( s.size()!= t.size()) return false;
-       for(int i = 0 ; i < s.size();i++){
-         hashmap1[s[i]-'a']++;
-         hashmap2[t[i]-'a']++;
-       }
-        return (hashmap1 == hashmap2);
+        
+        if(s.size() != t.size()) return false;
+        for( int i = 0; i<t.size(); i++){
+            hashmap1[s[i]-'a']++;
+            hashmap2[t[i]-'a']++;
+        }
+        
+        return hashmap1==hashmap2;
     }
 };
