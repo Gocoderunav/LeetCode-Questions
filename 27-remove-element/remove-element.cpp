5717e920877ced59def7146896d00d6568f16741
Gaurav Gangwar <120463239+Gocoderunav@users.noreply.github.com>
2024-05-30 20:28:21 +0530

    Time: 0 ms (100.00%) | Memory: 10.5 MB (5.84%) - LeetSync

--- 27-remove-element/remove-element.cpp
+++ 27-remove-element/remove-element.cpp
@@ -4,7 +4,7 @@ public:
         int count =0;
         for(int i = 0; i<nums.size();i++){
             if(nums[i]!=val){
-                swap(nums[i],nums[count]);
+                nums[count]=nums[i];
                 count++;
             }
         }
