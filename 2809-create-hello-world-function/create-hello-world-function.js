5a9bea7dd41a773a425c2aca83725f3f150c8a76
Gaurav Gangwar <120463239+Gocoderunav@users.noreply.github.com>
2024-06-27 17:19:53 +0530

    Time: 48 ms (71.55%) | Memory: 48.8 MB (37.01%) - LeetSync

---
+++ 2809-create-hello-world-function/create-hello-world-function.js
@@ -0,0 +1,18 @@
+/**
+ * @return {Function}
+ */
+var createHelloWorld = function() {
+    return f;
+    function f() {
+        return "Hello World";
+    }    
+}
+
+
+
+
+
+/**
+ * const f = createHelloWorld();
+ * f(); // "Hello World"
+ */
