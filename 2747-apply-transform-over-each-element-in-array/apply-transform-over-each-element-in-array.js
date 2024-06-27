f64e2da7f3ac2e24220615baa11ba3254990077b
Gaurav Gangwar <120463239+Gocoderunav@users.noreply.github.com>
2024-06-27 18:16:26 +0530

    Time: 62 ms (7.46%) | Memory: 48.9 MB (44.77%) - LeetSync

--- 2747-apply-transform-over-each-element-in-array/apply-transform-over-each-element-in-array.js
+++ 2747-apply-transform-over-each-element-in-array/apply-transform-over-each-element-in-array.js
@@ -4,9 +4,5 @@
  * @return {number[]}
  */
 var map = function(arr, fn) {
-    const res = new Array (arr.length);
-    for( const i in arr){
-        res[i] = fn(arr[i],Number(i));
-    }
-    return res;
+   return arr.map(fn);
 };
